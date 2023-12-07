7cc1dbbc6001f840bbafc525528e55c43cb6f76e
zjalthoff <zjalthoff@miners.utep.edu>
2023-12-07 10:35:06 -0700

    Decision Tree - Gender Update

--- Predictive_DT.py
+++ Predictive_DT.py
@@ -16,20 +16,22 @@ clustered_df['cluster'] = label_encoder.fit_transform(clustered_df['Cluster_Numb
 # print(clustered_df['cluster'].value_counts())
 # print(clustered_df['Cluster_Number'].value_counts())
 
-clustered_df2 = pd.get_dummies(clustered_df,columns=['Season','Category',
+clustered_df2 = pd.get_dummies(clustered_df,columns=['Gender','Season','Category',
                                                      'Subscription Status','Payment Method','Subscription Status']) # May have to replace Item Purchased with Category
 
 print(clustered_df2.columns)
 
 Y = clustered_df2['cluster']
-indep_vars = ['Season_Fall', 'Season_Spring', 'Season_Summer', 'Season_Winter','Category_Accessories',
+indep_vars = ['Gender_Female', 'Gender_Male','Season_Fall', 'Season_Spring', 'Season_Summer', 'Season_Winter','Category_Accessories', # Likelihood of overfitting greatly increased with the addition of gender variables
        'Category_Clothing', 'Category_Footwear', 'Category_Outerwear',
         'Subscription Status_No', 'Subscription Status_Yes','Payment Method_Cash']
-X = clustered_df2[indep_vars] # what should the decision tree use to classify the age range of each customer?
+# # # Try not to use variables that mean the same thing
+
+X = clustered_df2[indep_vars]
 
 x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.2,random_state=1)
 
-# # # DECISION TREE ATTEMPT
+# # # DECISION TREE
 
 tree_depth = 25
 for i in range(1,tree_depth+1):
@@ -40,3 +42,7 @@ for i in range(1,tree_depth+1):
     precision_DT = precision_score(y_test,y_hat_DT,average='weighted')
     f_1_DT = f1_score(y_test,y_hat_DT,average='weighted')
     print(f"For max depth of {i}, precision is:{precision_DT}, F1 is:{f_1_DT}, and accuracy is:{accuracy_DT}")
+
+
+# # # RANDOM FOREST
+cust_RF = 
