6bc742f3e2e2fdf58a29e8e1d1117fabcbcf20fd
zjalthoff <zjalthoff@miners.utep.edu>
2023-12-09 11:36:48 -0700

    Predictive Model Results fix
    Fixed the text results that accompany each model's results

--- Predictive_DT_Z.py
+++ Predictive_DT_Z.py
@@ -12,6 +12,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
 
+warnings.filterwarnings('ignore')
+
 # # # Preparing the Dataframe
 
 df = pd.read_csv('_shopping_trends_with_three_clusters.csv')
@@ -29,8 +31,7 @@ clustered_df2 = pd.get_dummies(clustered_df,columns=['Gender','Season','Category
 #print(clustered_df2.columns)
 
 Y = clustered_df2['cluster']
-indep_vars = ['Gender_Male','Season_Fall', 'Season_Spring', 'Season_Summer', 'Season_Winter','Category_Accessories', # Likelihood of overfitting greatly increased with the addition of gender variables
-       'Category_Clothing', 'Category_Footwear', 'Category_Outerwear', 'Subscription Status_Yes', 'Payment Method_Cash']
+indep_vars = ['Subscription Status_Yes']
 # # # Try not to use variables that mean the same thing
 
 X = clustered_df2[indep_vars]
@@ -52,43 +53,43 @@ for i in range(1,tree_depth+1):
 
 # # # RANDOM FOREST - All evaluation metrics return 1 w/ current variables (Risk of overfitting)
 
-# cust_RF = RandomForestClassifier(random_state=0)
-# cust_RF.fit(x_train,y_train)
-# y_hat_RF = cust_RF.predict(x_test)
-# accuracy_RF = accuracy_score(y_test,y_hat_RF)
-# precision_RF = precision_score(y_test,y_hat_RF,average='weighted')
-# f_1_RF = f1_score(y_test,y_hat_RF,average='weighted')
-# print(f"Using the Random Forest method, precision is:{precision_RF}, F1 is {f_1_RF}, and accuracy is {accuracy_RF}")
+cust_RF = RandomForestClassifier(random_state=0)
+cust_RF.fit(x_train,y_train)
+y_hat_RF = cust_RF.predict(x_test)
+accuracy_RF = accuracy_score(y_test,y_hat_RF)
+precision_RF = precision_score(y_test,y_hat_RF,average='weighted')
+f_1_RF = f1_score(y_test,y_hat_RF,average='weighted')
+print(f"Using the Random Forest method, precision is:{precision_RF}, F1 is {f_1_RF}, and accuracy is {accuracy_RF}")
 
 
 # # # SVM - All evaluation metrics return 1 w/ current variables (Risk of overfitting)
 
-# cust_SVM = SVC(gamma='auto')
-# cust_SVM.fit(x_train,y_train)
-# y_hat_SVM = cust_SVM.predict(x_test)
-# accuracy_SVM = accuracy_score(y_test,y_hat_SVM)
-# precision_SVM = precision_score(y_test,y_hat_SVM,average='weighted')
-# f_1_SVM = f1_score(y_test,y_hat_SVM,average='weighted')
-# print(f"Using the Random Forest method, precision is:{precision_SVM}, F1 is {f_1_SVM}, and accuracy is {accuracy_SVM}")
+cust_SVM = SVC(gamma='auto')
+cust_SVM.fit(x_train,y_train)
+y_hat_SVM = cust_SVM.predict(x_test)
+accuracy_SVM = accuracy_score(y_test,y_hat_SVM)
+precision_SVM = precision_score(y_test,y_hat_SVM,average='weighted')
+f_1_SVM = f1_score(y_test,y_hat_SVM,average='weighted')
+print(f"Using the SVC method, precision is:{precision_SVM}, F1 is {f_1_SVM}, and accuracy is {accuracy_SVM}")
 
 
 # # # Logistic Regression - Less than perfect, but very close to perfect w/ current variables (Risk of overfitting)
 
-# cust_LR = LogisticRegression(solver='liblinear',random_state=1)
-# cust_LR.fit(x_train,y_train)
-# y_hat_LR = cust_LR.predict(x_test)
-# accuracy_LR = accuracy_score(y_test,y_hat_LR)
-# precision_LR = precision_score(y_test,y_hat_LR,average='weighted')
-# f_1_LR = f1_score(y_test,y_hat_LR,average='weighted')
-# print(f"Using the Random Forest method, precision is:{precision_LR}, F1 is {f_1_LR}, and accuracy is {accuracy_LR}")
+cust_LR = LogisticRegression(solver='liblinear',random_state=1)
+cust_LR.fit(x_train,y_train)
+y_hat_LR = cust_LR.predict(x_test)
+accuracy_LR = accuracy_score(y_test,y_hat_LR)
+precision_LR = precision_score(y_test,y_hat_LR,average='weighted')
+f_1_LR = f1_score(y_test,y_hat_LR,average='weighted')
+print(f"Using the Logistic Regression method, precision is:{precision_LR}, F1 is {f_1_LR}, and accuracy is {accuracy_LR}")
 
 
 # # # Naïve Bayes - Less than perfect, but very close to perfect w/ current variables (Risk of overfitting)
 
-# cust_NB = GaussianNB()
-# cust_NB.fit(x_train,y_train)
-# y_hat_NB = cust_NB.predict(x_test)
-# accuracy_NB = accuracy_score(y_test,y_hat_NB)
-# precision_NB = precision_score(y_test,y_hat_NB,average='weighted')
-# f_1_NB = f1_score(y_test,y_hat_NB,average='weighted')
-# print(f"Using the Random Forest method, precision is:{precision_NB}, F1 is {f_1_NB}, and accuracy is {accuracy_NB}")
+cust_NB = GaussianNB()
+cust_NB.fit(x_train,y_train)
+y_hat_NB = cust_NB.predict(x_test)
+accuracy_NB = accuracy_score(y_test,y_hat_NB)
+precision_NB = precision_score(y_test,y_hat_NB,average='weighted')
+f_1_NB = f1_score(y_test,y_hat_NB,average='weighted')
+print(f"Using the Naïve Bayes method, precision is:{precision_NB}, F1 is {f_1_NB}, and accuracy is {accuracy_NB}")
